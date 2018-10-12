492e48d795900fa5f280c0cd40056f1a1ad21958
Datalytyx-Data-Science <43178517+Datalytyx-Data-Science@users.noreply.github.com>
2018-10-12 14:22:10 +0100

    Updated Setup Files and Architecture
    Changed setup.py in root directory, added __init__.py to dlxtools, updated readme.

--- README.rst
+++ README.rst
@@ -4,7 +4,8 @@ Datalytyx Data Science Tools
 
 **This is the unofficial Datalytyx Data Science Team code repository**
 
+
 dlxtools Module
 ===============
 
-This module contains wrapper around sklearn transformers, also including custom transformers, that Datalytyx employees should use to construct pipelines.
+This module contains wrappers around sklearn transformers, also including custom transformers, that Datalytyx employees should use to construct pipelines.
--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name = 'dlxtools',
+with open('README.rst', 'r') as fh:
+	long_description = fh.read()
+
+setup(name = 'dlxds',
       version = '0.1',
       description = 'Datalytyx data science tools',
       url = 'https://github.com/Datalytyx-Data-Science/dlxds',
       author = 'Datalytyx data science team',
       author_email = '',
       license = 'MIT',
-      packages = ['dlxtools'],
+      packages = find_packages(),
       install_requires = ['numpy', 'pandas', 'sklearn', 'scipy'],
-      long_description = open(README.md).read(),
-      zip_safe=False)
+      long_description = long_description,
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"])
