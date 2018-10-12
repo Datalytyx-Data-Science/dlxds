bfedf2106ea7a933805dfaa155e716c237bbd6b8
Datalytyx-Data-Science <43178517+Datalytyx-Data-Science@users.noreply.github.com>
2018-10-12 14:47:41 +0100

    Code cosmetics in setup

--- setup.py
+++ setup.py
@@ -4,14 +4,19 @@ with open('README.rst', 'r') as fh:
 	long_description = fh.read()
 
 setup(name = 'dlxds',
-      version = '0.1',
+      version = '0.0.1',
       description = 'Datalytyx data science tools',
       url = 'https://github.com/Datalytyx-Data-Science/dlxds',
       author = 'Datalytyx data science team',
       author_email = '',
       license = 'MIT',
       packages = find_packages(),
-      install_requires = ['numpy', 'pandas', 'sklearn', 'scipy', 'os', 'warnings'],
+      install_requires = ['numpy',
+      					 'pandas',
+      					 'sklearn', 
+      					 'scipy', 
+      					 'os', 
+      					 'warnings'],
       long_description = long_description,
       zip_safe=False,
       classifiers=[
