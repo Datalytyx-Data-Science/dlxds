d7b0a7f63e9c68b757f9e4e035fc70d141c3f16a
Datalytyx-Data-Science <43178517+Datalytyx-Data-Science@users.noreply.github.com>
2018-10-18 16:21:34 +0100

    Added DataFrameFeatureUnion to pipelines module

--- dlxtools/pipelines.py
+++ dlxtools/pipelines.py
@@ -3,3 +3,66 @@ This is a pipelines module
 
 Use for creating and saving commonly used pipelines.
 """
+
+#Pipeline construction tools
+#===========================
+
+class DataFrameFeatureUnion(BaseEstimator, TransformerMixin):
+    """ A DataFrame transformer that unites several DataFrame transformers
+    
+    Fit several DataFrame transformers and provides a concatenated
+    Data Frame
+    
+    Parameters
+    ----------
+    list_of_transformers : list of DataFrameTransformers
+        
+        
+    Authors
+    -------
+    Guy who wrote this one: https://www.kaggle.com/jankoch/scikit-learn-pipelines-and-pandas
+    """ 
+    def __init__(self, list_of_transformers):
+        self.list_of_transformers = list_of_transformers
+        
+    def transform(self, X, **transformparamn):
+        """ Applies the fitted transformers on a DataFrame
+        
+        Parameters
+        ----------
+        X : pandas DataFrame
+        
+        Returns
+        ----------
+        concatted :  pandas DataFrame
+        
+        """
+        
+        concatted = pd.concat([transformer.transform(X)
+                            for transformer in
+                            self.fitted_transformers_], axis=1).copy()
+        return concatted
+
+
+    def fit(self, X, y=None, **fitparams):
+        """ Fits several DataFrame Transformers
+        
+        Parameters
+        ----------
+        X : pandas DataFrame
+        y : not used, API requirement
+        
+        Returns
+        ----------
+        self : object
+        """
+        
+        self.fitted_transformers_ = []
+        for transformer in self.list_of_transformers:
+            fitted_trans = clone(transformer).fit(X, y=None, **fitparams)
+            self.fitted_transformers_.append(fitted_trans)
+        return self
+    
+    
+#Custom Prebuilt Transformers
+#============================
