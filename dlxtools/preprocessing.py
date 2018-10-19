2ba848275372e82283007c5605c099f6569ae443
Datalytyx-Data-Science <43178517+Datalytyx-Data-Science@users.noreply.github.com>
2018-10-19 16:21:07 +0100

    Tidied preprocessing/ added Sparse feature dropper

--- dlxtools/preprocessing.py
+++ dlxtools/preprocessing.py
@@ -4,27 +4,39 @@ Module for preprocessing tools.
 Class List (in order):
 DataFrameSelector
 """
-
+#===========================================================================================
 #Column Selectors
-#=================
+#===========================================================================================
 
 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     """
     Description:
-    A selection tranformer that will select columns of specific datatypes,
-    e.g. numeric, categoric etc..
+    ------------
+    A selection tranformer that will select columns of specified Pandas datatypes:
+    
+    Pandas          |Python  |Used For
+    ----------------+--------+---------------------------------
+    object          |str     |Text
+    int64           |int     |Integer numbers
+    float64         |float   |Floating point numbers
+    bool            |bool    |True/False values
+    datetime64      |NA      |Date and time values
+    timedelta[ns]   |NA      |Differences between two datetimes
+    category        |NA      |Finite list of text values
     
     
     Authors:
+    --------
     Chris Schon
     Eden Trainor
     
+
     TODO:
-    Allow for choice to return numpy array or pandas Dataframe
+    -----
     """
     
-    def __init__(self, attribute_names, returning = 'DataFrame'):
+    def __init__(self, attribute_name):
         """
         Description
         -----------
@@ -33,17 +45,17 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
         Args
         ----
         attribute_names: string
-        
+            Columns coresponding to this attribute will be selected.
         """
+        self.attribute_name = attribute_name
         
-        self.attribute_names = attribute_names
-    
+ 
     def fit(self, X, y=None):
         """
         Description
         -----------
         No fitting required for this transformer.
-        Simply checks that input is a pandas dataframe.
+        Simply checks that input is a pandas dataframe or a numpy array.
         
         Args
         ----
@@ -59,7 +71,8 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
             Returns the fitted transformer object.
         """
         
-        assert isinstance(X, pd.DataFrame)
+
+        assert isinstance(X, (pd.DataFrame, np.ndarray))
         
         return self
     
@@ -67,7 +80,7 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
         """
         Description
         -----------
-        
+        Selects and returns columns of pandas data type attribute_name
         
         Args
         ----
@@ -76,15 +89,107 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
             
         Returns
         -------
+        X_typed: DataFrame, (examples, features)
+            Pandas DataFrame containing columns of only  
+        """
+        
+        X_typed = X.select_dtypes(attribute_name)
+        
+        return X_typed
+   
+
+#===========================================================================================
+#Data Cleaners
+#===========================================================================================
+
+class SparseFeatureDropper(TransformerMixin, BaseEstimator):
+    """
+    Description:
+    ------------
+    Transformer drops feature columns with a certain percentage of empty rows. 
+    The user can set the threshold for this.
+    
+    
+    Authors:
+    --------
+    Eden Trainor
+
+    TODO:
+    -----
+
+    """
+     
+    def __init__(self, set_threshold = 90):
+        """
+        Description
+        -----------
+        Initialise the transformer object.
+        
+        Args
+        ----
+        set_threshold: int
+            Intiger percentage of empty empty rows in a column.
+        """
+
+        self.set_threshold = set_threshold
+        
+        
+    def fit(self, X, y = None): #has to take an optional y for pipelines
+        """
+        Description
+        -----------
+        Calculates the number of missing values the corresponds to the threshold.
+        Detects and labels columns with more missing values that the threshold.
+        
+        Args
+        ----
+        X: DataFrame, (examples, features)
+            Pandas DataFrame containing training example features
+            
+        y: array/DataFrame, (examples,)
+            (Optional) Training example labels
+            
+        Returns
+        -------
         self: sklearn.transfromer object
             Returns the fitted transformer object.
         """
-    
-        return X[self.attribute_names].values
-    
+
+
+        #Threshold defined by # full bins in df.dropna(), we've defined  threshold as percentage empty bins.
+        absolute_threshold = (100 - self.set_threshold)*X.shape[0]/100 
+        
+        
+        self.drop_columns = features.isna().sum()[features.isna().sum() > absolute_threshold].index #Calculates pd.series with column lables as indecies
+        
+        return self
 
     
-#=======    
+    def transform(self, X):
+        """
+        Description
+        -----------
+        Drops columns with more missing values than the threshold.
+
+        Args
+        ----
+        X: DataFrame, (examples, features)
+            Pandas DataFrame containing training/test example features
+
+        Returns
+        -------
+        X_full: DataFrame, (examples, features)
+            Pandas DataFrame containing example features without columns in drop_columns            
+        """   
+        
+        assert isinstance(X, pd.DataFrame)
+        
+        X_full = X.drop(columns = self.drop_columns)
+
+        return X_full
+
+    
+#===========================================================================================    
 #Scalers
-#=======
+#===========================================================================================
 
